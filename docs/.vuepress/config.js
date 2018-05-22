9ca7af466ff40169f34c8a38582de72a21fe20d8
lipengzhou <lpzmail@163.com>
2018-05-22 20:59:40 +0800

    :tada: Initial commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  base: '/',
+  title: '微信小程序入门教程',
+  description: '微信小程序入门教程',
+  port: 8080,
+  themeConfig: {
+    repo: 'lipengzhou/miniprogram-tutorial',
+    lastUpdated: '上次更新',
+    docsDir: 'docs',
+    editLinks: true,
+    editLinkText: '在 GitHub 上编辑此页',
+    nav: [
+      { text: '基础', link: '/guide/' }
+    ],
+    sidebar: {
+      '/guide/': [
+      ]
+    }
+  }
+}
---
+++ docs/README.md
@@ -0,0 +1,7 @@
+---
+home: true
+heroImage: /logo.png
+actionText: 开始 →
+actionLink: /guide/
+footer: MIT Licensed | Copyright © 2018-present lpz
+---
